e939adf2b8416a0c7b19cd74e1a8d030f22fc89c
i.khurani <huran438@gmail.com>
2024-12-01 20:55:59 +0300

    Improved Settings logic

--- Runtime/SFProjectSettings.cs
+++ Runtime/SFProjectSettings.cs
@@ -1,38 +1,48 @@
 #pragma warning disable CS0162
-using System.IO;
 using UnityEngine;
 
 namespace SFramework.Core.Runtime
 {
     public abstract class SFProjectSettings<T> : ScriptableObject where T : SFProjectSettings<T>
     {
-        private const string _folderPath = "SFramework/Settings";
-        public static readonly string _assetPath = $"Assets/{_folderPath}/{typeof(T).Name}.asset";
+        private static T _instance;
 
         public static bool Instance(out T result)
         {
             result = null;
-            
+
 #if UNITY_EDITOR
-            var path = Path
-                .GetFullPath(Path.Combine(Application.dataPath, _folderPath));
-
-            if (!Directory.Exists(path))
+            if (_instance == null)
             {
-                Directory.CreateDirectory(path);
-                UnityEditor.AssetDatabase.Refresh();
+                var settings = Resources.Load<T>(typeof(T).Name);
+                if (settings == null)
+                {
+                  
+                    
+                    if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/SFramework"))
+                    {
+                        UnityEditor.AssetDatabase.CreateFolder("Assets", "SFramework");
+                    }
+                    
+                    if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/SFramework/Resources"))
+                    {
+                        UnityEditor.AssetDatabase.CreateFolder("Assets/SFramework", "Resources");
+                    }
+                    
+                    UnityEditor.AssetDatabase.CreateAsset(CreateInstance<T>(), "Assets/SFramework/Resources/" + typeof(T).Name + ".asset");
+                    UnityEditor.AssetDatabase.ImportAsset("Assets/SFramework/Resources/" + typeof(T).Name + ".asset", UnityEditor.ImportAssetOptions.ForceUpdate);
+                    UnityEditor.AssetDatabase.Refresh();
+                    settings = Resources.Load<T>(typeof(T).Name);
+                }
+
+                result = settings;
+                _instance = result;
             }
-
-            var settings = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(_assetPath);
-            if (settings == null)
+            else
             {
-                settings = CreateInstance<T>();
-                UnityEditor.AssetDatabase.CreateAsset(settings, _assetPath);
-                UnityEditor.AssetDatabase.SaveAssetIfDirty(settings);
-                UnityEditor.AssetDatabase.Refresh();
+                result = _instance;
             }
             
-            result = settings;
             return true;
 #endif
             return false;
